1f0d449b1079b0fffc356de72522535c1b860b68
Zach Leatherman <zachleatherman@gmail.com>
2022-11-14 17:02:57 -0600

    Use the demo from the readme

--- demo.js
+++ demo.js
@@ -4,23 +4,23 @@ let feed = new ActivityFeed();
 
 feed.setCacheDuration("4h");
 
-feed.addSource("youtubeUser", "YouTube", "UCskGTioqrMBcw8pd14_334A"); // Eleventy
-feed.addSource("youtubeUser", "YouTube", "UCMlSs0Ltg57qpYdFwUVLR2A"); // zachleat
+// YouTube
+feed.addSource("youtubeUser", "YouTube", "UCskGTioqrMBcw8pd14_334A");
 
-feed.addSource("atom", "Blog","https://www.zachleat.com/web/feed/");
+// Blog
 feed.addSource("atom", "Blog", "https://www.11ty.dev/blog/feed.xml");
 
-feed.addSource("rss", "Mastodon", "https://fediverse.zachleat.com/users/zachleat.rss");
+// Mastodon
 feed.addSource("rss", "Mastodon", "https://fosstodon.org/users/eleventy.rss");
 
-feed.addSource("twitterUser", "Twitter", "zachleat", "96383");
+// Twitter
 feed.addSource("twitterUser", "Twitter", "eleven_ty", "949639269433380864");
 
 let content = await feed.toRssFeed({
-	title: "Zach’s Activity Feedbag",
+	title: "Eleventy’s Activity Feed",
 	language: "en",
-	url: "https://www.zachleat.com/feed/activity/",
-	subtitle: "A feed of all of Zach’s socials in one.",
+	url: "https://www.11ty.dev/follow/",
+	subtitle: "One centralized feed of Eleventy activity across the web.",
 });
 
 console.log( content );
